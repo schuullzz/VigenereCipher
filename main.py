b000cc3d13278ad48aaa750103892c59f4adaf91
Timothy Schultz <59267968+schuullzz@users.noreply.github.com>
2021-06-26 12:49:00 -0500

    Commit #3: Put algorithms into functions and created a menu

--- main.py
+++ main.py
@@ -7,65 +7,120 @@
 
 import re
 
-# Class for building a circular list
+# *************************************************************************************************************
+
+character_dict = {
+    "a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9, "k": 10, "l": 11,
+    "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18, "t": 19, "u": 20, "v": 21, "w": 22,
+    "x": 23, "y": 24, "z": 25
+}
+
+character_dict_cipher = {
+    0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h", 8: "i", 9: "j", 10: "k", 11: "l",
+    12: "m", 13: "n", 14: "o", 15: "p", 16: "q", 17: "r", 18: "s", 19: "t", 20: "u", 21: "v", 22: "w",
+    23: "x", 24: "y", 25: "z"
+}
+
+# *************************************************************************************************************
 
 
-class Node:
+def plain_to_cipher():
+    # Prompt user for sentence to encrypt and the key.
+    sentence = str(input("Enter a Plain Text to encrypt: "))
+    key = str(input("Enter a key: "))
 
-    def __init__(self, char):
-        self.char = char
-        self.next = None
+    # Changes string to lowercase.
+    sentence = sentence.lower()
+    key = key.lower()
 
+    # Regular expression that removes all upper case and non alphabetical characters.
+    regular_expression = re.compile('[^a-za-z]')
+    sentence = regular_expression.sub('', sentence)
+    key = regular_expression.sub('', key)
 
-class CircularList:
+    # Convert Sentence and key to lists
+    sentence = list(sentence)
+    key = list(key)
+    difference = list()
+    index = 0
+    cipher_sentence = ""
 
-    def __init__(self):
-        self.head = None
+    for x in range(len(sentence)):
+        if index == len(key):
+            index = 0
 
-    def add(self, char):
-        hold = Node(char)
-        temp = self.head
-        hold.next = self.head
-
-        if self.head is not None:
-            while temp.next != self.head:
-                temp = temp.next
-            temp.next = hold
+        if character_dict[key[index]] == character_dict[sentence[x]]:
+            difference.append(0)
+        elif (character_dict[key[index]] + character_dict[sentence[x]]) > 25:
+            difference.append(character_dict[key[index]] + character_dict[sentence[x]] - 25)
         else:
-            hold.next = hold
+            difference.append(character_dict[key[index]] + character_dict[sentence[x]])
 
-        self.head = hold
+        index = index + 1
+        cipher_sentence += character_dict_cipher[difference[x]]
+
+    print("Cipher Text: " + cipher_sentence)
+
+# *************************************************************************************************************
 
 
-print("Vigenere Cipher\n")
+def cipher_to_plain():
+    # Prompt user for sentence to encrypt and the key.
+    cipher_text = str(input("Enter a Cipher Text to decrypt: "))
+    key = str(input("Enter a key: "))
 
-# Prompt user for sentence to encrypt and the key.
-sentence = str(input("Enter a sentence to encrypt: "))
-key = str(input("Enter a key: "))
+    # Changes string to lowercase.
+    cipher_txt = cipher_text.lower()
+    key = key.lower()
 
-# Coverts all alphabetic letters to lower case.
-sentence = sentence.lower()
-key = key.lower()
+    # Regular expression that removes all upper case and non alphabetical characters.
+    regular_expression = re.compile('[^a-za-z]')
+    cipher_text = regular_expression.sub('', cipher_txt)
+    key = regular_expression.sub('', key)
 
-# Regular expression that removes all upper case and non alphabetical characters.
-regular_expression = re.compile('[^a-za-z]')
-sentence = regular_expression.sub('', sentence)
-key = regular_expression.sub('', key)
+    # Convert Sentence and key to lists
+    cipher_text = list(cipher_text)
+    key = list(key)
+    difference = list()
+    index = 0
+    plain_sentence = ""
 
-# Convert Sentence and key to lists
-sentence = list(sentence)
-key = list(key)
-difference = list()
-sentenceList = CircularList()
-index = 0
+    for x in range(len(cipher_text)):
+        if index == len(key):
+            index = 0
 
-for x in range(len(sentence)):
-    sentenceList.add(sentence[x])
+        if character_dict[key[index]] == character_dict[cipher_text[x]]:
+            difference.append(0)
+        elif (character_dict[cipher_text[x]] - character_dict[key[index]]) < 0:
+            difference.append(character_dict[cipher_text[x]] - character_dict[key[index]] + 25)
+        else:
+            difference.append(character_dict[cipher_text[x]] - character_dict[key[index]])
 
-    if index == (len(key) - 1):
-        index = 0
+        index = index + 1
+        plain_sentence += character_dict_cipher[difference[x]]
 
-    difference.append(ord(key[index]) + ord(sentence[x]))
-    index = index + 1
+    print("Plain Text: " + plain_sentence)
 
-print(difference)
+# *************************************************************************************************************
+
+print("Project 1: Vigenere Cipher\n")
+
+while True:
+    print("Select an option: ")
+    print("1. Task One: Encryption")
+    print("2. Task One: Decryption")
+    print("3. Quit Program\n")
+
+    answer = int(input("Option: "))
+    print("")
+
+    if answer == 1:
+        plain_to_cipher()
+        print("")
+    elif answer == 2:
+        cipher_to_plain()
+        print("")
+    elif answer == 3:
+        break
+    else:
+        print("Incorrect option try again.\n")
